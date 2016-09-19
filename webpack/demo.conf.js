0fd23e6b84edde64c15f442695444a81e08faf90
H.Alper Tuna <halpertuna@gmail.com>
2016-09-19 05:20:28 +0300

    Added build script for demo page

---
+++ webpack/demo.conf.js
@@ -0,0 +1,35 @@
+/*
+ * @file demo.conf.js
+ * @author H.Alper Tuna <halpertuna@gmail.com>
+ * Date: 09.09.2016
+ */
+
+/* eslint import/no-extraneous-dependencies: ["error", {"devDependencies": true}] */
+
+const webpack = require('webpack');
+const config = require('./base.conf');
+
+// Webpack Production Settings - Minified
+module.exports = Object.assign(config, {
+  entry: './dev/App.jsx',
+  output: {
+    path: './',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      ...config.module.loaders,
+      {
+        test: /\.less$/,
+        loader: 'style?insertAt=top!css!less',
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+      },
+    }),
+  ],
+});
