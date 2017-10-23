f96a1e1bcebf3db2345aba01249c06de13438594
Jason Wong <js0n@devb0x.local>
2017-10-23 14:25:07 -0400

    added unit test for Item component to increase code coverage

--- src/containers/Item.js
+++ src/containers/Item.js
@@ -9,7 +9,7 @@ import { changeSubMenuVisibility, changeActiveLinkId } from '../actions/content'
 import { emitSelected } from '../actions/emitters';
 import Item from '../components/Item';
 
-const mapDispatchToProps = (dispatch, ownProps) => ({
+export const mapDispatchToProps = (dispatch, ownProps) => ({
   toggleSubMenu(e) {
     if (!ownProps.hasSubMenu) return;
     e.preventDefault();
