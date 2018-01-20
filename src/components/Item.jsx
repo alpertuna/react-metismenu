6480a7fbe563d7bd8680b2ebdc08513ade4e1a62
josephzhao <zhaoshuilin2004@yahoo.ca>
2018-01-20 11:26:22 -0500

    Update Item.jsx

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -50,7 +50,7 @@ const Item = ({
       activateMe={activateMe}
     >
       <i className={classnames(classStore.classIcon, classStore.iconNamePrefix + icon)} />
-      <span class='content'>{label}</span>
+      <span className='content'>{label}</span>
       {hasSubMenu && <span><i
         className={classnames(
           classStore.classStateIcon,
