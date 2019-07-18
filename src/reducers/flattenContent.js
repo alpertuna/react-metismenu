34039dc0906c595f3843acf665c174963bbea604
jpribyl <pribylsnbits@gmail.com>
2019-07-18 14:12:29 -0600

    updated to match formatting existing in master of main repo

--- src/reducers/flattenContent.js
+++ src/reducers/flattenContent.js
@@ -41,6 +41,9 @@ const mapTrace = (content, parentId) => {
     item.trace = [...trace];
     trace.push(item.id);
     item.hasSubMenu = mapTrace(content, item.id);
+    if (item.hasSubMenu) {
+      item.to = '#';
+    }
     trace.pop();
   });
   return subItems.length > 0;
